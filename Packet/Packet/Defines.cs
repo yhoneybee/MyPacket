e106e9bff2e44e9eda6bc845abebb9fadff5ccc9
yhoneybee <chaeyh04@gmail.com>
2022-05-27 20:28:47 +0900

    < update > size fix

--- Packet/Packet/Defines.cs
+++ Packet/Packet/Defines.cs
@@ -10,7 +10,7 @@ namespace MyPacket
     {
         public static readonly int KILO = 1024;
 
-        public static readonly int MESSAGE_BUFFER_SIZE = KILO * 2000;
+        public static readonly int MESSAGE_BUFFER_SIZE = KILO * 20;
         public static readonly int HEADER_BUFFER_SIZE = 4;
         public static readonly int TYPE_BUFFER_SIZE = 2;
     }
